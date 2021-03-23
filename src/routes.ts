c3d2fb86cb24cdde10356216fd335446ba59b7b6
gfbatista <gilbertoferrari@gmail.com>
2021-03-23 16:46:42 -0300

    swagger fixes

--- src/routes.ts
+++ src/routes.ts
@@ -19,72 +19,12 @@ const routes = Router();
 
 routes.use('/zipcode/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
-/**
- * @swagger 
- * /zipcode/health/ping:
- *  get:
- *      description: Checagem da conexão da aplicação
- *      responses:
- *          '200':
- *              description: Connected to the database.
- *          '500':
- *              description: Database not connected.
- */
 routes.get('/zipcode/health/ping', checkConnection.ping);
 
 routes.post('/zipcode/token', sessionTokenController.create);
 
-/**
- * @swagger 
- * /zipcode/{cep}:
- *  get: 
- *     description: Busca de um determinado CEP 
- *     parameters: 
- *       - 
- *         in: path
- *         name: cep
- *         required: true
- *         schema: 
- *           format: string
- *     responses:
- *          '200':
- *              description: Ok 
- *          '400':
- *              description: Bad Request           
- *          '401':
- *              description: Unauthorized 
- */
 routes.get('/zipcode/:cep', Authorization, zipCodeController.find);
 
-/**
- * @swagger 
- * /zipcode:
- *  post:
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             type: object
- *             properties:
- *               cep:
- *                 type: string 
- *               rua:
- *                 type: string   
- *               bairro:
- *                 type: string 
- *               cidade:
- *                 type: string 
- *               uf:
- *                 type: string 
- *     responses:
- *          '201':
- *              description: Created
- *          '400':
- *              description: Bad Request           
- *          '401':
- *              description: Unauthorized  
- */
 routes.post('/zipcode', Authorization, zipCodeController.create);
 
 export default routes;
--- src/util/swagger.ts
+++ src/util/swagger.ts
@@ -1,11 +1,106 @@
 export default {
     definition: {
-        openapi: '3.0.5',
+        swagger: "2.0",
         info: {
-            version: '1.0.0',
-            title: 'busca-cep-api',
-            description: 'API de cadastro e busca de CEP'
-        }
+            version: "1.0.0",
+            title: "busca-cep-api"
+        },
+        paths: {
+            "/zipcode/health/ping": {
+                get: {
+                    description: "Checagem da conexão da aplicação",
+                    responses: {
+                        200: {
+                            description: "Connected to the database"
+                        },
+                        500: {
+                            description: "Database not connected"
+                        }
+                    }
+                }
+            },
+            "/zipcode/{cep}": {
+                get: {
+                    description: " Busca de um determinado CEP",
+                    parameters: [
+                        {
+                            name: "Authorization",
+                            in: "header",
+                            type: "string",
+                            required: true
+                        },
+                        {
+                            in: "path",
+                            name: "cep",
+                            required: "true",
+                            schema: {
+                                format: "string"
+                            }
+                        }
+                    ],
+                    responses: {
+                        200: {
+                            description: "Ok"
+                        },
+                        400: {
+                            description: "Bad Request"
+                        },
+                        401: {
+                            description: "Unauthorized "
+                        }
+                    }
+                }
+            },
+            "/zipcode": {
+                post: {
+                    description: "Cadastro de um novo CEP",
+                    parameters: [
+                        {
+                            name: "Authorization",
+                            in: "header",
+                            type: "string",
+                            required: true
+                        },
+                        {
+                            in: "body",
+                            name: "body",
+                            required: true,
+                            schema: {
+                                type: "object",
+                                properties: {
+                                    cep: {
+                                        type: "string"
+                                    },
+                                    rua: {
+                                        type: "string"
+                                    },
+                                    bairro: {
+                                        type: "string"
+                                    },
+                                    cidade: {
+                                        type: "string"
+                                    },
+                                    uf: {
+                                        type: "string"
+                                    },
+                                }
+                            }
+                        }
+                    ],
+                    responses: {
+                        201: {
+                            description: "Created"
+                        },
+                        400: {
+                            description: "Bad Request"
+                        },
+                        401: {
+                            description: "Unauthorized "
+                        }
+                    }
+                }
+            }
+        },
     },
     apis: ["./src/routes.ts"],
 };
